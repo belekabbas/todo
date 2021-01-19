113edaa97f8aad6e96379d4420c1eb52998278a5
belek10 <belekabbas@gmail.com>
2021-01-19 20:23:44 +0600

    added todo and book form

--- main/models.py
+++ main/models.py
@@ -3,9 +3,18 @@ from django.db import models
 
 class ToDo(models.Model):
     text = models.CharField(max_length = 100)
-    created_at = models.DateField(auto_now_add = True)
+    created_at = models.DateTimeField(auto_now_add = True)
     is_closed = models.BooleanField(default = False)
     is_favorite = models.BooleanField(default = False)
 
+class BookShop(models.Model):
+    title = models.CharField(max_length = 150)
+    subtitle = models.CharField(max_length = 300)
+    description = models.TextField(blank = True)
+    price = models.DecimalField(max_digits = 6, decimal_places = 2)
+    genre = models.CharField(max_length = 100)
+    author = models.CharField(max_length = 50)
+    year = models.DateField(auto_now_add = False, auto_now = False)
+    date = models.DateField(auto_now_add = True)
 
 # Create your models here.
--- main/views.py
+++ main/views.py
@@ -1,14 +1,39 @@
-from django.shortcuts import render, HttpResponse
-from .models import ToDo
+from django.shortcuts import render, HttpResponse, redirect
+from .models import ToDo, BookShop
 
 def homepage(request):
     return render(request, "index.html")
+
 
 def test(request):
     todo_list = ToDo.objects.all()
     return render(request, "test.html", {"todo_list": todo_list})
 
+
 def second(request):
-    return HttpResponse("test 2 page")
+    books = BookShop.objects.all()
+    return render(request, "books.html", {"books": books})
+
+
+def add_todo(request):
+    form = request.POST
+    text = form['todo_text']
+    todo = ToDo(text=text)
+    todo.save()
+    return redirect(test)
+
+
+def add_book(request):
+    form = request.POST
+    title = form['book_title']
+    subtitle = form['book_subtitle']
+    description = form['book_description']
+    price = form['book_price']
+    genre = form['book_genre']
+    author = form['book_author']
+    year = form['book_year']
+    book = BookShop(title = title,subtitle = subtitle,description = description,price = price,genre = genre,author = author,year = year)
+    book.save()
+    return redirect(book)
 
 
